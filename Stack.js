6810d0d8e0d91efaec62560c8fb8c23433bb429f
HoangAnh <nguyenhoanganh05122003@gmail.com>
2024-06-27 17:05:13 +0700

    commit stack

---
+++ Stack.js
@@ -0,0 +1,6 @@
+// LIFO : last in fisrt out
+// Item nào vào sau cùng thì item đó được lấy ra đầu tiên
+// Các loại Stack : Simple Array Implementation, Dynamic Array Implementation, Linked List Implementation
+// Kể cả có bao nhiêu implementation đi nữa thì các implementation đều phải tuân thủ ADT (Abstract Data Type)
+// Đối với những loại cấu trúc dữ liệu thì nó luôn luôn có thứ gọi là Abstract Data Type, có nghĩa là dù cho tạo implementation của stack bằng linked list hay array hay là gì đi nữa
+// Nhưng mà nó luôn luôn phải có những function sau (đối với stack) : Push(), Pop(), Top(), Size(),isStackEmpty(), isFullstack()
